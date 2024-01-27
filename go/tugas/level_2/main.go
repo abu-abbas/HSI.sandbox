cb5cbb93e55dca047bbfcccbce460f040047b1fc
abu-abbas <notificationfor.gitlabs@gmail.com>
2024-01-27 17:29:38 +0700

    fix(tugas): Perbaikan error handling saat nil dijadikan parameter generateNik

--- go/tugas/level_2/main.go
+++ go/tugas/level_2/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"log"
 	"sort"
@@ -22,10 +23,11 @@ func main() {
 	fmt.Println("Generate NIK Akhwat:", nikAkhwat, "\n")
 
 	// generate NIK Lanjutan
-	adminIkhwan1, _ := generateNIKLanjutan("ARN171-07770", 2)
-	adminIkhwan2, _ := generateNIKLanjutan("ARN181-08880", 2)
-	adminAkhwat1, _ := generateNIKLanjutan("ART191-06660", 2)
-	adminAkhwat2, _ := generateNIKLanjutan("ART201-05550", 2)
+	var nikN1, nikN2, nikT1, nikT2 string = "ARN171-07770", "ARN181-08880", "ART191-06660", "ART201-05550"
+	adminIkhwan1, _ := generateNIKLanjutan(&nikN1, 2)
+	adminIkhwan2, _ := generateNIKLanjutan(&nikN2, 2)
+	adminAkhwat1, _ := generateNIKLanjutan(&nikT1, 2)
+	adminAkhwat2, _ := generateNIKLanjutan(&nikT2, 2)
 
 	fmt.Println("Generate NIK Lanjutan (Ikhwan):", adminIkhwan1, adminIkhwan2, "\n")
 	fmt.Println("Generate NIK Lanjutan (Akhwat):", adminAkhwat1, adminAkhwat2, "\n")
@@ -38,6 +40,10 @@ func main() {
 		adminAkhwat1,
 		adminAkhwat2,
 	)
+
+	// generate error NIK Lanjutan
+	_, errLanjutan := generateNIKLanjutan(nil, 2)
+	fmt.Println("Display error:", errLanjutan)
 }
 
 func generateNIK(gender string, tahun int, jumlah int) ([]string, error) {
@@ -45,8 +51,12 @@ func generateNIK(gender string, tahun int, jumlah int) ([]string, error) {
 	return result, err 
 }
 
-func generateNIKLanjutan(nik string, jumlah int) ([]string, error) {
-	gender, tahun, smt, urutan := generator.ParseNIK(nik)
+func generateNIKLanjutan(nik *string, jumlah int) ([]string, error) {
+	if nik == nil {
+		return nil, errors.New("nik tidak boleh nil")	
+	}
+
+	gender, tahun, smt, urutan := generator.ParseNIK(*nik)
 	result, err := generator.NIK(gender, tahun, jumlah, urutan, smt)
 	return result, err
 }
